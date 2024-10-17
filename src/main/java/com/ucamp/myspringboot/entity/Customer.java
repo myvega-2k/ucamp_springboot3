7b9c9f02d6c4724a9a9c5837a55a2a3f846f7409
myvega-2k <myvega2k@gmail.com>
2024-10-17 15:11:39 +0900

    user entity repo add

--- src/main/java/com/ucamp/myspringboot/entity/Customer.java
+++ src/main/java/com/ucamp/myspringboot/entity/Customer.java
@@ -3,10 +3,13 @@ package com.ucamp.myspringboot.entity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.annotations.DynamicUpdate;
 
 @Entity
 @Table(name = "customers")
 @Getter @Setter
+@DynamicUpdate
 public class Customer {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
--- src/test/java/com/ucamp/myspringboot/repository/CustomerRepositoryTest.java
+++ src/test/java/com/ucamp/myspringboot/repository/CustomerRepositoryTest.java
@@ -4,22 +4,43 @@ import com.ucamp.myspringboot.entity.Customer;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
+@Transactional
 class CustomerRepositoryTest {
     @Autowired
     CustomerRepository customerRepository;
 
     @Test
+    //Rollback 하지 마세요
+    @Rollback(value = false)
     void save_find() {
         Customer customer = new Customer();
-        customer.setCustomerId("A001");
+        customer.setCustomerId("A002");
         customer.setCustomerName("스프링");
         Customer saved = customerRepository.save(customer);
         assertThat(saved).isNotNull();
         assertThat(saved.getCustomerName()).isEqualTo("스프링");
+        Optional<Customer> optionalById =
+                customerRepository.findById(1L);//Optional<Customer>
+        //assertThat(optionalById).isNotEmpty();
+        if(optionalById.isPresent()){
+            Customer exist = optionalById.get();
+            assertThat(exist.getCustomerId()).isEqualTo("A001");
+            //update는 setter만 호출해도 업데이트가 됩니다.
+            exist.setCustomerName("스프링부트");
+            //delete
+            //customerRepository.delete(exist);
+        }
+
+
+
     }
 }
