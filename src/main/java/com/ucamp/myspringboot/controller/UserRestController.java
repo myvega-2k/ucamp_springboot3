038578143d06de8905b73a0aa89a5dcc51782da3
myvega-2k <myvega2k@gmail.com>
2024-10-17 16:15:14 +0900

    user controller add

---
+++ src/main/java/com/ucamp/myspringboot/controller/UserRestController.java
@@ -0,0 +1,37 @@
+package com.ucamp.myspringboot.controller;
+
+import com.ucamp.myspringboot.entity.User;
+import com.ucamp.myspringboot.exception.BusinessException;
+import com.ucamp.myspringboot.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/users")
+public class UserRestController {
+    private final UserRepository userRepository;
+
+    //Constructor Injection
+//    public UserRestController(UserRepository userRepository) {
+//        this.userRepository = userRepository;
+//    }
+
+    @PostMapping
+    public User addUser(@RequestBody User user){
+        return userRepository.save(user);
+    }
+
+    @GetMapping("/{id}")
+    public User getUser(@PathVariable Long id){
+        Optional<User> optionalUser = userRepository.findById(id);
+        return optionalUser.orElseThrow(
+                () -> new BusinessException("User Not Found", HttpStatus.NOT_FOUND)
+        );
+    }
+
+
+}
