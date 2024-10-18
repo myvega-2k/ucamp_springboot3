886750d79a9d85c2f3d9397d7b2943ba4444f57a
myvega-2k <myvega2k@gmail.com>
2024-10-18 09:36:26 +0900

    controller delete add

--- src/main/java/com/ucamp/myspringboot/controller/UserRestController.java
+++ src/main/java/com/ucamp/myspringboot/controller/UserRestController.java
@@ -5,6 +5,7 @@ import com.ucamp.myspringboot.exception.BusinessException;
 import com.ucamp.myspringboot.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -28,8 +29,13 @@ public class UserRestController {
 
     @GetMapping("/{id}")
     public User getUser(@PathVariable Long id){
-        Optional<User> optionalUser = userRepository.findById(id);
-        return optionalUser.orElseThrow(
+        //Optional<User> optionalUser = userRepository.findById(id);
+        return getUserNotFound(id);
+
+    }
+
+    private User getUserNotFound(Long id) {
+        return userRepository.findById(id).orElseThrow(
                 () -> new BusinessException("User Not Found", HttpStatus.NOT_FOUND)
         );
     }
@@ -46,6 +52,13 @@ public class UserRestController {
 
         existUser.setName(userDetail.getName());
         return userRepository.save(existUser);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteUser(@PathVariable Long id) {
+        User existUser = getUserNotFound(id);
+        userRepository.delete(existUser);
+        return  ResponseEntity.ok("Id = " + id + " User가 삭제 되었습니다.");
     }
 
 }
